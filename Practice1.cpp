039fa8cfda3a6fdec483596ed888fa6cabb6150f
YU-CHEN LIANG <59174634+liang890626@users.noreply.github.com>
2023-04-22 22:59:27 +0800

    Update Practice1.cpp

--- Practice1.cpp
+++ Practice1.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 using namespace std;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main() {
 	double length = 0.0, width = 0.0;
 	
