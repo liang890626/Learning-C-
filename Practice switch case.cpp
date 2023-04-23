3692fb67462ac1cf10fb1d13e919c1d78edfee94
YU-CHEN LIANG <59174634+liang890626@users.noreply.github.com>
2023-04-23 18:57:33 +0800

    Update Practice switch case.cpp

--- Practice switch case.cpp
+++ Practice switch case.cpp
@@ -11,7 +11,6 @@
 #include <iostream>
 using namespace std;
 
-
 int main() {
     int year, month, days;
 
@@ -65,5 +64,46 @@ int main() {
 
     return 0;
 }
+
+
+//Same question using if-else statement:
+#include <iostream>
+using namespace std;
+
+int main() {
+    int year, month, days;
+    bool isLeapYear;
+    
+    cout << "Enter year: ";
+    cin >> year;
+    
+    cout << "Enter month (1-12): ";
+    cin >> month;
+    
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+        isLeapYear = true;
+    } else {
+        isLeapYear = false;
+    }
+    
+    if (month == 2) {
+        if (isLeapYear) {
+            days = 29;
+        } else {
+            days = 28;
+        }
+    } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        days = 30;
+    } else {
+        days = 31;
+    }
+    
+    cout << "There are " << days << " days in month " << month << " of year " << year << "." << endl;
+    
+    return 0;
+}
+
+
+
 
  
